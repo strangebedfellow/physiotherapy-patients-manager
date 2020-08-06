c88be077c32bdb8bcd22697c137313388e318603
Mirosław Pajor <54025053+strangebedfellow@users.noreply.github.com>
2020-08-06 22:52:38 +0200

    todo

--- src/components/FillDb.js
+++ src/components/FillDb.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import * as firebase from 'firebase';
 
+//testing db ref for push()
+// for now adds new visit with below data
+//#############to change: open modal with inputs to add new visit
 const FillDb = (id) => {
     console.log('fill');
     const rootRef = firebase.database().ref('patients').child(id).child("visits");
