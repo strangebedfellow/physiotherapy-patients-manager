3cde03fb9e714216947ffaba37422e487def50b5
Mirosław Pajor <54025053+strangebedfellow@users.noreply.github.com>
2020-09-22 13:40:11 +0200

    update

--- src/components/UpdatePatientData.js
+++ src/components/UpdatePatientData.js
@@ -52,7 +52,6 @@ export default function UpdatePatientData(props) {
                 variant="contained"
                 color="secondary"
                 size="small"
-                // startIcon={<EditIcon />}
                 onClick={handleClickOpen}
             >
                 <EditIcon />
