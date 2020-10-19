9be48f75022d506778229183cbe7e249d87af5e0
Mirosław Pajor <54025053+strangebedfellow@users.noreply.github.com>
2020-10-19 14:55:06 +0200

    handle errors

--- src/components/AppLogin.js
+++ src/components/AppLogin.js
@@ -3,6 +3,8 @@ import * as firebase from 'firebase';
 import { auth } from "firebase";
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
+import Paper from '@material-ui/core/Paper';
 
 function AppLogin() {
   const initialState = {
@@ -23,18 +25,30 @@ function AppLogin() {
       })
       .catch(function (error) {
         var errorCode = error.code;
-        var errorMessage = error.message;
-        console.log(errorCode)
+        //var errorMessage = error.message;
+        if (errorCode === 'auth/invalid-email') {
+          alert('Błędny adres e-mail')
+        }
+        if (errorCode === 'auth/user-not-found') {
+          alert('Nie znaleziono użytkownika')
+        }
+        if (errorCode === 'auth/wrong-password') {
+          alert('Błędne hasło')
+        }
       });
   }
 
   return (
     <div className='app-login'>
-      <form noValidate autoComplete="off">
-        <TextField name="email" onChange={handleChange} id="standard-basic" label="użytkownik" />
-        <TextField name="password" onChange={handleChange} id="standard-basic" label="hasło" type="password" />
-        <Button onClick={signIn} variant="contained" color="primary">Zaloguj</Button>
-      </form>
+      <Paper elevation={4} variant="outlined">
+        <Box px={5} width={250}>
+          <form noValidate autoComplete="off">
+            <TextField name="email" onChange={handleChange} id="standard-basic" label="użytkownik" fullWidth/>
+            <TextField name="password" onChange={handleChange} id="standard-basic" label="hasło" type="password" />
+            <Button onClick={signIn} variant="contained" color="primary">Zaloguj</Button>
+          </form>
+        </Box>
+      </Paper >
     </div>
   );
 }
